9071c229e4d3f11a048dc535d0f8f58d766b7cf0
efmomoh <efmomoh25@gmail.com>
2026-07-31 22:13:36 +0800

    Adds homepage layout, navigation and search bar

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+/*
+==========================================================
+
+Country Explorer App
+
+Main JavaScript Entry File
+
+Purpose:
+This file starts our application.
+Every other JavaScript module will eventually
+be imported here.
+
+==========================================================
+*/
+
+console.log("Country Explorer App Started");
