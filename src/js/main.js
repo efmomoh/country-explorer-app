9b91232147e5cc702722c7b487e7484a90209d81
efmomoh <efmomoh25@gmail.com>
2026-08-02 06:12:45 +0800

    feat: integrate country search module with REST Countries API

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,13 @@ be imported here.
 ========================================================== */
 import CountryExplorerApp from './CountryExplorerApp.mjs';
 // Temporary API testing
-import './api-test.mjs';
-import './api-country-model-test.mjs';
+// import './api-test.mjs';
+// import './api-country-model-test.mjs';
+// import './search-test.mjs';
 
 // Create the Application Object
+// eslint-disable-next-line
 const app = new CountryExplorerApp();
 
 // Start the Application
-app.initialize();
+// app.initialize();
